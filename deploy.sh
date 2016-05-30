97342fcea402027f08c9a84177659e33bf754734
nixoxo <nico.axtmann95@gmail.com>
2016-05-30 13:44:38 +0200

    added api, org and space in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
+#curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 
-./cf login --u ${CF_EMAIL} -p ${CF_PASSWORD}
+./cf login --u ${CF_EMAIL} -p ${CF_PASSWORD} -a https://api.run.pivotal.io -o bookster -s development
 ./cf push -f ./deploy/cloudfoundry/manifest.yml -p build/libs/bookster-2-0.0.1-SNAPSHOT.war
