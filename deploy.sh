1b5504f27abbe5e457fdbe52a42389afe7e03af6
nixoxo <nico.axtmann95@gmail.com>
2016-05-30 13:15:45 +0200

    changed deploy.sh env

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 
-./cf login --u ${cf.email} -p ${cf.password}
+./cf login --u ${CF_EMAIL} -p ${CF_PASSWORD}
 ./cf push -f ./deploy/cloudfoundry/manifest.yml -p build/libs/bookster-2-0.0.1-SNAPSHOT.war
