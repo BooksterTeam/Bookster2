04fc2ed73cd525ec7fceb9ac2211e6df8546fc5a
nixoxo <nico.axtmann95@gmail.com>
2016-06-02 22:30:54 +0200

    Remixed

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 git branch
+
 ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
 git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
-ls -l
+
 cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
 
-git checkout -b master
-git push origin :master
+ls -l
+
 git push foobar master
