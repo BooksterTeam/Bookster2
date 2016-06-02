bd986f72ed801694fc3874b2ebd9d47a3dc6b8a6
nixoxo <nico.axtmann95@gmail.com>
2016-06-02 15:03:48 +0200

    Add deploy script at other sever instance

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
-
-./cf login --u ${CF_EMAIL} -p ${CF_PASSWORD} -a https://api.run.pivotal.io -o bookster -s development
-./cf push -f ./deploy/cloudfoundry/manifest.yml -p build/libs/bookster-2-0.0.1-SNAPSHOT.war
+ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
+git remote add foobar git+ssh://dokku@bookster.bee.pw:foobar.bookster.bee.pw
+git push -u foobar master
