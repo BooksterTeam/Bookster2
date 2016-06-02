2b4d7f1e0608d7f8d2a36bef30392755e14bb965
nixoxo <nico.axtmann95@gmail.com>
2016-06-02 21:46:05 +0200

    Changed order

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 git branch
+ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
+git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
 rm -rf .gitignore
 git add .
 gcmsg "deployed"
-#ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
-git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
 git checkout -b master
 git push origin :master
 git push -u foobar master
