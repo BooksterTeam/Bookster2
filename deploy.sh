e96c69b9efc761543e6df2e2fb9d3165ed777a42
nixoxo <nico.axtmann95@gmail.com>
2016-06-02 23:52:19 +0200

    Last fix

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,10 @@
 git branch
 
 ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
-git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
+#git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
+git remote set-url origin dokku@bookster.bee.pw:foobar.bookster.bee.pw
 git branch
 cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
 #git pull master
+git push origin --delete master
 git push -u foobar master
