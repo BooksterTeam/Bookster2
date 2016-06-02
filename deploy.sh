04e2a30fe6772fc61dd157d73de6a975c5fd975a
nixoxo <nico.axtmann95@gmail.com>
2016-06-02 23:36:22 +0200

    Changed to git pull

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
 git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
 git branch
 cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
-git checkout master
+git pull master
 git push foobar master
