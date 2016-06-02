e985d82264c88eb3c117cb31f4989d6c8ff5565b
nixoxo <nico.axtmann95@gmail.com>
2016-06-02 22:43:58 +0200

    works?

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
 git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
 
 cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
-
 ls -l
 
-git push foobar master
+
+git push autodeploy master
