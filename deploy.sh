b4442c909a0900723a6525e4eb77ea5855c62f87
nixoxo <nico.axtmann95@gmail.com>
2016-06-02 22:58:26 +0200

    Changed checkout

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
 cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
 ls -l
 
-git checkout -b master
+git checkout -b autodeploy
 git push autodeploy master
