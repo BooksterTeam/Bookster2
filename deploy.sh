3c9111a565bd0e9c461760e02acfa3539314c6e3
nixoxo <nico.axtmann95@gmail.com>
2016-06-02 22:07:05 +0200

    Fixed gitignore for war file

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ git branch
 ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
 git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
 ls -l
-gcmsg "deployed"
+#cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
+
 git checkout -b master
 git push origin :master
 git push foobar master
