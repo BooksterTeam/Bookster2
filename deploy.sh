702eb399a2a7f9ac5684ce3417f6ef74e3847b2c
nixoxo <nico.axtmann95@gmail.com>
2016-06-02 19:18:01 +0200

    Changed branch

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git branch
 ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
 git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
+git checkout -b master
 git push -u foobar master
