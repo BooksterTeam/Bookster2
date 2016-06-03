a1c69635045e34a11b74e05792826b310f67d706
nixoxo <nico.axtmann95@gmail.com>
2016-06-03 09:26:03 +0200

    Created new folder

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,21 @@
 git branch
 
 ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
-git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
 
 
 cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
 
-git commit app.war
-git status
+cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ../app.war
+pwd
 ls -l
+cd ..
+git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
+git add app.war
+git commit app.war
 git push foobar master
+
+#git commit app.war
+#git status
+#ls -l
+#pwd
+#git push foobar master
