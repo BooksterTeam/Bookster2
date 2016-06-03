3a76f1543bdd1d05c9ea17c92e2cc66e89c4fdf0
nixoxo <nico.axtmann95@gmail.com>
2016-06-03 12:39:11 +0200

    Changed docker and deploy

--- deploy.sh
+++ deploy.sh
@@ -2,24 +2,22 @@
 git branch
 
 
-
-
 #cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
 
-#cd ..
-#mkdir build
-#cd build
-#cp ../Bookster2/build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
-#mv ../Bookster2/Dockerfile ./
-#ls -l
-#
+cd ..
+mkdir build
+cd build
+cp ../Bookster2/build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
+mv ../Bookster2/Dockerfile ./
+ls -l
+
 pwd
 
 ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
-rm -rf .git
-rm -rf .gitignore
+#rm -rf .git
+#rm -rf .gitignore
 
-cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
+
 git init
 git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
 git config --global user.email "dokku@asdfsadf.com"
