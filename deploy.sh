6aa4d2fabbba9ba998911044a634473bde81b7b2
nixoxo <nico.axtmann95@gmail.com>
2016-06-03 11:37:53 +0200

    Changed origin to foobar

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git config --global user.email "dokku@asdfsadf.com"
 git config --global user.name "dokku"
 git add -A
 git commit -m "Foobat"
-git push origin :master
+git push foobar :master
 git push -u foobar master
 
