e8cd6def1ee3508af91c8c226541609512b607f7
nixoxo <nico.axtmann95@gmail.com>
2016-06-03 11:58:28 +0200

    Changed something inside path

--- deploy.sh
+++ deploy.sh
@@ -6,23 +6,25 @@ git branch
 
 #cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
 
-cd ..
-mkdir build
-cd build
-cp ../Bookster2/build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
-mv ../Bookster2/Dockerfile ./
-ls -l
+#cd ..
+#mkdir build
+#cd build
+#cp ../Bookster2/build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
+#mv ../Bookster2/Dockerfile ./
+#ls -l
+#
 pwd
 
 ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
-
+rm -rf .git
+cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
 git init
 git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
 git config --global user.email "dokku@asdfsadf.com"
 git config --global user.name "dokku"
 git add -A
 git commit -m "Foobat"
-#git push foobar :master
+
 git fetch foobar
 git push -f foobar master
 
