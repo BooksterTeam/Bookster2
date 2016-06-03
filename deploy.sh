77f76aaade36635dfc83be194085e5cb58e069ee
nixoxo <nico.axtmann95@gmail.com>
2016-06-03 10:05:46 +0200

    Add app

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,7 @@ ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
 
 cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
 
-cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ../app.war
 pwd
-ls -l
-cd ..
-ls -l
-git init
 git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
 git add app.war
 git commit app.war
