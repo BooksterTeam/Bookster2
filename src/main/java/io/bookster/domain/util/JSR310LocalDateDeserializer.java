9fb83a85d60246a6ed338b2807d9fb7ba0217036
dan-brown <daniel.brown@live.de>
2016-06-03 14:52:39 +0200

    moved static constant up, refactored deserialize method

--- src/main/java/io/bookster/domain/util/JSR310LocalDateDeserializer.java
+++ src/main/java/io/bookster/domain/util/JSR310LocalDateDeserializer.java
@@ -18,9 +18,9 @@ public class JSR310LocalDateDeserializer extends JsonDeserializer<LocalDate> {
 
     public static final JSR310LocalDateDeserializer INSTANCE = new JSR310LocalDateDeserializer();
 
-    private JSR310LocalDateDeserializer() {}
-
     private static final DateTimeFormatter ISO_DATE_OPTIONAL_TIME;
+
+    private JSR310LocalDateDeserializer() {}
 
     static {
         ISO_DATE_OPTIONAL_TIME = new DateTimeFormatterBuilder()
@@ -33,31 +33,39 @@ public class JSR310LocalDateDeserializer extends JsonDeserializer<LocalDate> {
 
     @Override
     public LocalDate deserialize(JsonParser parser, DeserializationContext context) throws IOException {
-        switch(parser.getCurrentToken()) {
-            case START_ARRAY:
-                if(parser.nextToken() == JsonToken.END_ARRAY) {
-                    return null;
-                }
-                int year = parser.getIntValue();
-
-                parser.nextToken();
-                int month = parser.getIntValue();
-
-                parser.nextToken();
-                int day = parser.getIntValue();
-
-                if(parser.nextToken() != JsonToken.END_ARRAY) {
-                    throw context.wrongTokenException(parser, JsonToken.END_ARRAY, "Expected array to end.");
-                }
-                return LocalDate.of(year, month, day);
-
-            case VALUE_STRING:
-                String string = parser.getText().trim();
-                if(string.length() == 0) {
-                    return null;
-                }
-                return LocalDate.parse(string, ISO_DATE_OPTIONAL_TIME);
+        JsonToken jsonToken = parser.getCurrentToken();
+        if (jsonToken == JsonToken.START_ARRAY) {
+            return getLocalDateFromArray(parser, context);
+        } else if (jsonToken == JsonToken.VALUE_STRING) {
+            return getLocalDateFromString(parser);
+        } else {
+            throw context.wrongTokenException(parser, JsonToken.START_ARRAY, "Expected array or string.");
         }
-        throw context.wrongTokenException(parser, JsonToken.START_ARRAY, "Expected array or string.");
+    }
+
+    private LocalDate getLocalDateFromString(JsonParser parser) throws IOException {
+        String string = parser.getText().trim();
+        if (string.length() == 0) {
+            return null;
+        }
+        return LocalDate.parse(string, ISO_DATE_OPTIONAL_TIME);
+    }
+
+    private LocalDate getLocalDateFromArray(JsonParser parser, DeserializationContext context) throws IOException {
+        if (parser.nextToken() == JsonToken.END_ARRAY) {
+            return null;
+        }
+        int year = parser.getIntValue();
+
+        parser.nextToken();
+        int month = parser.getIntValue();
+
+        parser.nextToken();
+        int day = parser.getIntValue();
+
+        if (parser.nextToken() != JsonToken.END_ARRAY) {
+            throw context.wrongTokenException(parser, JsonToken.END_ARRAY, "Expected array to end.");
+        }
+        return LocalDate.of(year, month, day);
     }
 }
