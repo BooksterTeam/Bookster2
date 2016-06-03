716b01fd81a909967b9f5230f93d825cfbe48ea4
nixoxo <nico.axtmann95@gmail.com>
2016-06-03 11:01:49 +0200

    Changed build routine

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 git branch
 
+
+
+
+#cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
+
+cd ..
+mkdir build
+cd build
+cp ../Bookster2/build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
+mv ../Bookster2/Dockerfile ./
+ls -l
+pwd
+
 ssh-keyscan bookster.bee.pw >> ~/.ssh/known_hosts
 
-
-cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
-
-pwd
 git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
-#git add app.war
-#git commit app.war
+git add -A
+git commit -m "Foobat"
 git push foobar master
 
-#git commit app.war
-#git status
-#ls -l
-#pwd
-#git push foobar master
