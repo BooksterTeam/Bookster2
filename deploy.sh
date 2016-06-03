05372163fb05fe073080fb2edeb41c168a49009b
nixoxo <nico.axtmann95@gmail.com>
2016-06-03 09:16:20 +0200

    Add app.war

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,7 @@ git remote add foobar dokku@bookster.bee.pw:foobar.bookster.bee.pw
 
 cp build/libs/bookster-2-0.0.1-SNAPSHOT.war ./app.war
 
+git commit app.war
 git status
-
-git pull
-git checkout -b master
 ls -l
-git push -u foobar master
+git push foobar master
